897541e6f2ab6d812090828c14a1f0e4691067cc
fatima safa <fatimacivil1993@gmail.com>
2020-01-13 06:11:16 +0000

    paragraph to header

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ c=r.content
 soup=BeautifulSoup(c,"html.parser")
 all=soup.find_all("div",{"class":"cities"})
 for item in all:
-    print(item.find_all('p')[0].text)
+    print(item.find_all('h2')[0].text)
